7f6d0800f3b013bd129f50d2380ae4b890effb93
Renan Fernandes Silva <renanfssilva@gmail.com>
2020-12-19 16:00:12 -0300

    Generic Stacks Test

---
+++ Generic Stack/MyStack/MyStack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Stacks.Library
+{
+    public class MyStack<T>
+    {
+        private readonly List<T> _items = new List<T>();
+
+        public MyStack<T> Push(T item)
+        {
+            _items.Add(item);
+            
+            return this;
+        }
+
+        public T Pop()
+        {
+            if (_items.Count == 0)
+            {
+                return default;
+            }
+            
+            var item = _items[^1];
+            _items.RemoveAt(_items.Count - 1);
+            
+            return item;
+        }
+    }
+}
