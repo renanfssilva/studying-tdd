443360ed96841e9533a52799f1010f842965682f
Renan Fernandes Silva <renanfssilva@gmail.com>
2020-12-19 14:54:33 -0300

    FizzBuzz Tests

---
+++ FizzBuzz/FizzBuzz.Library/FizzBuzzService.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz.Library
+{
+    public class FizzBuzzService
+    {
+        public string Print(int n)
+        {
+            if (IsDivisibleByThree(n) && IsDivisibleByFive(n))
+            {
+                return "FizzBuzz";
+            }
+            
+            if (IsDivisibleByFive(n))
+            {
+                return "Buzz";
+            }
+
+            if (IsDivisibleByThree(n))
+            {
+                return "Fizz";
+            }
+
+            return n.ToString();
+        }
+
+        public bool IsDivisibleByThree(int n)
+        {
+            return n % 3 == 0;
+        }
+        
+        public bool IsDivisibleByFive(int n)
+        {
+            return n % 5 == 0;
+        }
+    }
+}
